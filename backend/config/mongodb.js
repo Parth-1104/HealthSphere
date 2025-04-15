c8ca4fbbb76903e8fe5036c158920a4d2635a94d
Parth-1104 <141064865+Parth-1104@users.noreply.github.com>
2025-04-15 18:04:17 +0530

    added updated the backend portin

--- backend/config/mongodb.js
+++ backend/config/mongodb.js
@@ -1,9 +1,12 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
-const connectDB=async ()=>{
+const connectDB = async () => {
 
-    mongoose.connection.on("connected",()=> console.log("Database Connected"))
-    await mongoose.connect(`${process.env.MONGODB_URI}/HeathSphere`)
-    
+    mongoose.connection.on('connected', () => console.log("Database Connected"))
+    await mongoose.connect(`${process.env.MONGODB_URI}/prescripto`)
+
 }
+
 export default connectDB;
+
+// Do not use '@' symbol in your databse user's password else it will show an error.
--- backend/models/doctorModel.js
+++ backend/models/doctorModel.js
@@ -1,16 +1,20 @@
 import mongoose from "mongoose";
-const doctorSchema=new mongoose.Schema({
-    name:{type:String,require:true},
-    email:{type:String,require:true,unique:true},
-    password:{type:String,require:true},
-    image:{type:String,require:true},
-    speciality:{type:String,require:true},
-    degree:{type:String,require:true},
-    experience:{type:String,require:true},
-    about:{type:String,require:true},
-    available:{type:Boolean,require:true},
-    fees:{type:Number,require:true},
-    address:{type:Object,require:true},
-    date:{type:Number,require:true},
-    slots_booked:{type:Object,default:{}}
-},{minimize:false})
+
+const doctorSchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    image: { type: String, required: true },
+    speciality: { type: String, required: true },
+    degree: { type: String, required: true },
+    experience: { type: String, required: true },
+    about: { type: String, required: true },
+    available: { type: Boolean, default: true },
+    fees: { type: Number, required: true },
+    slots_booked: { type: Object, default: {} },
+    address: { type: Object, required: true },
+    date: { type: Number, required: true },
+}, { minimize: false })
+
+const doctorModel = mongoose.models.doctor || mongoose.model("doctor", doctorSchema);
+export default doctorModel;
